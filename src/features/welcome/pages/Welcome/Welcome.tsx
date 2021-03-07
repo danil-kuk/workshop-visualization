6cfbd610a5c7cff6119519789eb60c871d029438
danil-kuk <danilkuk5@gmail.com>
2021-03-07 14:09:33 +0500

    Добавлен пример с картинкой

--- src/features/welcome/pages/Welcome/Welcome.tsx
+++ src/features/welcome/pages/Welcome/Welcome.tsx
@@ -1,5 +1,6 @@
 import { Link, NavLink, Outlet } from "react-router-dom"
 import { publicUrlPath } from "src/utils/publicUrlPath"
+import logo from 'src/assets/images/logo-for-light.svg'
 
 import styles from './style.module.scss'
 
@@ -7,15 +8,23 @@ export const Welcome: React.FC = () => (
   <div>
     <h1>Welcome page</h1>
 
-    <NavLink
-      to={publicUrlPath('/welcome/1')}
-      activeClassName={styles.activeLink}
-    >
+    <div>
+      <NavLink
+        to={publicUrlPath('/welcome/1')}
+        activeClassName={styles.activeLink}
+      >
       NavLink to child route
-    </NavLink>
-    <br />
-    <Link to={publicUrlPath('/empty')}>Link to empty url</Link>
-
+      </NavLink>
+    </div>
+    <div>
+      <Link to={publicUrlPath('/empty')}>Link to empty url</Link>
+    </div>
+    <div className={styles.logoWrapper}>
+      <img
+        src={logo}
+        alt="logo"
+      />
+    </div>
     <h3>Router Outlet</h3>
     <Outlet />
   </div>
